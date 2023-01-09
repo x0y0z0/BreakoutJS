6dfccfa75dd7611105a35072ecc105f09713d86d
Sergey <kochetof.sergey@gmail.com>
2023-01-09 17:48:59 +0300

    array methods to game.js

--- game.js
+++ game.js
@@ -1,8 +1,34 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+/* 
+ДЛЯ ДОБАВЛЕНИЯ/УДАЛЕНИЯ ЭЛЕМЕНТОВ МАССИВА
+*/
+push(...items) // добавляет элементы в конец
+pop() // извлекает элемент с конца
+shift() // извлекает элемент с начала
+unshift(...items) // добавляет элементы в начало
+splice(pos, deleteCount, ...items) // начиная с [pos], удаляет deleteCount элементов и вставляет items
+slice(start, end) // создаёт новый массив, копируя в него [start, end)
+concat(...items) // возвращает новый массив из элементов текущего массива + items
+/* 
+ДЛЯ ПОИСКА СРЕДИ ЭЛЕМЕНТОВ МАССИВА
+*/
+indexOf(item, pos) //
+lastIndexOf(item, pos) //
+includes(value) // 
+find(func) //
+filter(func) //
+findIndex() //
+/*
+ДЛЯ ПЕРЕБОРА ЭЛЕМЕНТОВ МАССИВА
+*/
+forEach(func) //
+/*
+ДЛЯ ПРЕОБРАЗОВАНИЯ МАССИВА
+*/
+map(func) //
+sort(func) //
+reverse() //
+split / join //
+reduce(func, initial)
+reduceRight(func, initial)
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+Array.isArray(arr) // 
